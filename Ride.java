700c22a598244402c7a78f6c54b143a76814a89a
sneha2061 <nepalsneha2004@gmail.com>
2024-11-29 22:32:37 +1000

    updated ride class by implementing interface

--- Ride.java
+++ Ride.java
@@ -1,10 +1,18 @@
-public class Ride {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+public class Ride implements RideInterface {
     private String rideName;
     private int capacity;
     private Employee operator; // Employee assigned to this ride
+ private Queue<Visitor> visitorQueue;                   // Queue for waiting visitors
+    private List<Visitor> rideHistory;                     // List to store ride history
 
     // Default constructor
     public Ride() {
+     this.visitorQueue = new LinkedList<>();
+        this.rideHistory = new ArrayList<>();   
     }
 
     // Parameterized constructor
@@ -12,6 +20,8 @@ public class Ride {
         this.rideName = rideName;
         this.capacity = capacity;
         this.operator = operator;
+        this.visitorQueue = new LinkedList<>();
+        this.rideHistory = new ArrayList<>();
     }
 
     // Getters and Setters
@@ -37,5 +47,79 @@ public class Ride {
 
     public void setOperator(Employee operator) {
         this.operator = operator;
+    }
+    // Implementing RideInterface methods
+
+    @Override
+    public void addVisitorToQueue(Visitor visitor) {
+        visitorQueue.add(visitor);
+        System.out.println(visitor.getName() + " has been added to the queue for " + rideName);
+    }
+
+    @Override
+    public void removeVisitorFromQueue() {
+        if (!visitorQueue.isEmpty()) {
+            Visitor visitor = visitorQueue.poll();
+            System.out.println(visitor.getName() + " has been removed from the queue for " + rideName);
+        } else {
+            System.out.println("The queue is empty!");
+        }
+    }
+
+    @Override
+    public void printQueue() {
+        if (!visitorQueue.isEmpty()) {
+            System.out.println("Visitors in the queue for " + rideName + ":");
+            for (Visitor visitor : visitorQueue) {
+                System.out.println(visitor.getName());
+            }
+        } else {
+            System.out.println("No visitors in the queue for " + rideName);
+        }
+    }
+
+    @Override
+    public void runOneCycle() {
+        System.out.println("Running one cycle of " + rideName + "...");
+        int riders = 0;
+
+        while (!visitorQueue.isEmpty() && riders < capacity) {
+            Visitor visitor = visitorQueue.poll();
+            rideHistory.add(visitor);
+            System.out.println(visitor.getName() + " is riding " + rideName);
+            riders++;
+        }
+
+        if (riders == 0) {
+            System.out.println("No visitors to ride the " + rideName);
+        }
+    }
+
+    @Override
+    public void addVisitorToHistory(Visitor visitor) {
+        rideHistory.add(visitor);
+        System.out.println(visitor.getName() + " has been added to the ride history for " + rideName);
+    }
+
+    @Override
+    public boolean checkVisitorFromHistory(Visitor visitor) {
+        return rideHistory.contains(visitor);
+    }
+
+    @Override
+    public int numberOfVisitors() {
+        return rideHistory.size();
+    }
+
+    @Override
+    public void printRideHistory() {
+        if (!rideHistory.isEmpty()) {
+            System.out.println("Visitors who have ridden " + rideName + ":");
+            for (Visitor visitor : rideHistory) {
+                System.out.println(visitor.getName());
+            }
+        } else {
+            System.out.println("No visitors have ridden " + rideName + " yet.");
+        }
     }
 }
