95689504dbc43d6b206dcb2cb9d24f2ac67e0b7d
sneha2061 <nepalsneha2004@gmail.com>
2024-11-29 22:34:01 +1000

    updated main class by demonstration
    adding visitors to queue, printing ride history, etc.

--- AssignmentTwo.java
+++ AssignmentTwo.java
@@ -2,6 +2,19 @@ public class AssignmentTwo {
     public static void main(String[] args) {
         // Entry point for the PRVMS
         System.out.println("Welcome to Park Rides Visitor Management System (PRVMS)");
+        Employee operator = new Employee("Pratik", 30, "Redondo Street", "Ride Operator", 45000);
+        Ride rollerCoaster = new Ride("Roller Coaster", 5, operator);
+
+        Visitor visitor1 = new Visitor("Janice", 35, "Down street", "Regular", 1);
+        Visitor visitor2 = new Visitor("Patricia", 22, "Uptown", "Regular", 2);
+
+        rollerCoaster.addVisitorToQueue(visitor1);
+        rollerCoaster.addVisitorToQueue(visitor2);
+
+        rollerCoaster.printQueue();
+        rollerCoaster.runOneCycle();
+
+        rollerCoaster.printRideHistory();
     }
     public void partThree() {
         // Code part three 
@@ -27,3 +40,4 @@ public class AssignmentTwo {
         // Code part seven
     }
 }
+
