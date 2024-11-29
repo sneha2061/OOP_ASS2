268a8b06320860d6d3b583afe9c912494644d664
sneha2061 <nepalsneha2004@gmail.com>
2024-11-29 22:47:02 +1000

    updated the main class with part 3 demonstration

--- AssignmentTwo.java
+++ AssignmentTwo.java
@@ -1,24 +1,39 @@
 public class AssignmentTwo {
     public static void main(String[] args) {
         // Entry point for the PRVMS
-        System.out.println("Welcome to Park Rides Visitor Management System (PRVMS)");
-        Employee operator = new Employee("Pratik", 30, "Redondo Street", "Ride Operator", 45000);
-        Ride rollerCoaster = new Ride("Roller Coaster", 5, operator);
+        System.out.println("Welcome to Park Rides Visitor Management System (PRVMS)!!");
 
-        Visitor visitor1 = new Visitor("Janice", 35, "Down street", "Regular", 1);
-        Visitor visitor2 = new Visitor("Patricia", 22, "Uptown", "Regular", 2);
-
-        rollerCoaster.addVisitorToQueue(visitor1);
-        rollerCoaster.addVisitorToQueue(visitor2);
-
-        rollerCoaster.printQueue();
-        rollerCoaster.runOneCycle();
-
-        rollerCoaster.printRideHistory();
-    }
-    public void partThree() {
-        // Code part three 
-    }
+            AssignmentTwo assignment = new AssignmentTwo();
+            assignment.partThree();
+        }
+    
+        public void partThree() {
+            // Create an operator for the ride
+            Employee operator = new Employee("Emiliano", 30, "Park Street", "Ride Operator", 45000);
+    
+            // Create a new Ride object
+            Ride ferrisWheel = new Ride("Ferris Wheel", 3, operator);
+    
+            // Create Visitor objects
+            Visitor visitor1 = new Visitor("Alicia", 25, "Chinatown", "Regular", 1);
+            Visitor visitor2 = new Visitor("Barbara", 28, "Under street", "VIP", 2);
+            Visitor visitor3 = new Visitor("Charles", 22, "City Center", "Regular", 3);
+            Visitor visitor4 = new Visitor("David", 30, "Subrine street", "Regular", 4);
+            Visitor visitor5 = new Visitor("Evelyn", 27, "Downtown", "VIP", 5);
+    
+            // Add Visitors to the Queue
+            ferrisWheel.addVisitorToQueue(visitor1);
+            ferrisWheel.addVisitorToQueue(visitor2);
+            ferrisWheel.addVisitorToQueue(visitor3);
+            ferrisWheel.addVisitorToQueue(visitor4);
+            ferrisWheel.addVisitorToQueue(visitor5);
+    
+            // Remove a Visitor from the Queue
+            ferrisWheel.removeVisitorFromQueue();
+    
+            // Print all Visitors in the Queue
+            ferrisWheel.printQueue();
+        }
 
     public void partFourA() {
         // Code part 4A
